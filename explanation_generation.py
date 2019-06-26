14a8fe0ac4ac0dc870ed4dc6f3bb6e31d831aa90
keleas <grishin.na@phystech.edu>
2019-06-27 02:14:02 +0300

    first working generation (Arseny)

--- explanation_generation.py
+++ explanation_generation.py
@@ -4,7 +4,7 @@ import numpy as np
 # A dictionary with relations
 with open("./relations_dict", 'rb') as f:
     relations = pkl.load(f)
-	
+
 templates = dict()
 templates['/r/RelatedTo'] = ["is related to {}", "has a certain connection with {}", "is kind of related to {}", "is linked to {}", "has some relationship with nine"]
 templates['/r/IsA'] = ["is {}", "is an example of {}", "is a special case of {}", "is an instance of {}", "can be thought as an instance of {}", "could be considered to be an example of {}"]
@@ -13,7 +13,7 @@ templates['/r/HasA'] = ["has {} as a part", "has {} as its part"]
 templates['/r/UsedFor'] = ["is sometimes used for {}", "could be used for {}", "may be used for {}", "may prove its utilitarian value in {}", "{} is its purpose", "has {} as its purpose"]
 templates['/r/CapableOf'] = ["can {}", "is capable of {}", "can perform {}", "can do {}"]
 templates['/r/Causes'] = ["can cause {}", "may causes {}", "may be a cause of {}", "may lead to {}"]
-templates['/r/HasProperty'] = ["has a property that {}", "is characterizes by {}", "is {}"]
+templates['/r/HasProperty'] = ["has a property that {}", "is characterized by {}", "is {}"]
 templates['/r/Desires'] = ["wants (if not craves) {}", "desires {}", "wants {}", "is craving (or, at least, wants)"]
 templates['/r/Synonym'] = ["is a synonym for {}", "is synonimous to {}", "is essentially the same as {}", "has the same meaning as {}"]
 templates['/r/Antonym'] = ["is an antonym for {}", "is antonymous to {}", "is the opposite of {}", "has a meaning opposite to {}", "has an opposite meaning to {}"]
@@ -26,27 +26,42 @@ templates['/r/InstanceOf'] = ["is {}", "is an example of {}", "is a special case
 
 
 
-def get_hint(rel, source, target):
-	
-	return np.random.choice(templates[source]).format(target)
-	
-starters = ("It is a thing that ", "It is something that ", "This thing is such that ")
-connectors = (" and such that ", ", such that ", ", such that also ", ", also, ", ", additionally, ", ". Moreover, this thing is such that ")
+def get_hint(rel, target):
+
+    return np.random.choice(templates[rel]).format(target)
+
+starters = ("It is a thing that ", "It is something that ", "This thing is such that it")
+connectors = (" and such that it ", ", such that it ", ", such that it also ", ", also, it ", ", additionally, it ", ", moreover, this thing is such that it ")
+hesitations = ("ummmm ", "hmmm ", "ummm-hmmm ")
 
 def sample_definition(word):
-	
-	message = np.random.choice(starters)
 
-	relations = relations[word]
-	num_properties = np.min(np.random.geometric(0.5), len(relations[word]))
-	properties = np.random.choice(a, size=num_properties, replace=False)
-	
+    message = np.random.choice(starters)
 
-	for p in properties:
-		message += p
+    rels = relations[word]
+    num_properties = np.random.geometric(0.5) + 1
+    sampled_rels = np.random.choice(list(rels), size=num_properties, replace=True)
 
-	message += "."
+    hesitation_flag = False
+
+    for j, r in enumerate(sampled_rels):
+        if j > 0 and not hesitation_flag:
+            message += np.random.choice(connectors)
+
+        phrase = np.random.choice(relations[word][r])
+        if word in phrase:
+            message += np.random.choice(hesitations)
+            hesitation_flag = True
+        else:
+            message += get_hint(r, phrase)
+            hesitation_flag = False
+
+    message += "."
 
 
-	return message
-		
+    return message
+
+if __name__ == "__main__":
+
+    print(sample_definition("cat"))
+    print(sample_definition("god"))
--- parse_conceptnet.py
+++ parse_conceptnet.py
@@ -2,49 +2,56 @@ import json
 import os
 from collections import defaultdict
 import pickle as pkl
-import settings
 
 def getRelationDict():
     file_loc = "./conceptnet-assertions-5.6.0.csv"
-	
+
     symmetric_rels = {'/r/RelatedTo', '/r/Synonym', '/r/Antonym', '/r/DistinctFrom',
-					  '/r/SimilarTo', '/r/LocatedNear', '/r/EtymologicallyRelatedTo'}
+                      '/r/SimilarTo', '/r/LocatedNear', '/r/EtymologicallyRelatedTo'}
 
     with open(file_loc, 'r') as f:
-	
-		R = defaultdict(lambda: [])
-	
-		for i, line in enumerate(f):
-	
-			if not i % 1000000:
-				print("Processed {} lines".format(i))
-			
-			elements = line.split(sep='\t')
-			if len(elements) != 5:
-				continue
-	
-			rel, source, target, js  = elements[1:5]
-	
-			lang_source, source_w = source.split('/')[2:4]
-			lang_target, target_w = target.split('/')[2:4]
-	
-			if rel in ['/r/RelatedTo', '/r/IsA', '/r/PartOf', '/r/HasA', '/r/UsedFor', '/r/CapableOf', '/r/Causes', '/r/HasProperty', '/r/Desires', '/r/Synonym', '/r/Antonym', '/r/Entails', '/r/DistinctFrom', '/r/MannerOf', '/r/HasPrerequisite', '/r/SimilarTo', '/r/InstanceOf']:
-			#	for start in ['the ', 'an ', 'a ']:
-			#		if source_w.startswith(start):
-			#			source_w = source_w[len(start):]
-			#		if target_w.startswith(start):
-			#			target_w = target_w[len(start):]
-						
-				if lang_source == "en" and lang_target == "en":
-					
-					R[source_w].add((rel, target_w))
-	
-					if rel in symmetric_rels:
-					
-						R[target_w].add((rel, source_w))
 
-		for k in R:
-			R[k] = list(R[k])
-	
+        R = defaultdict(lambda: defaultdict(lambda: set()))
+
+        for i, line in enumerate(f):
+
+            if not i % 1000000:
+                print("Processed {} lines".format(i))
+
+            elements = line.split(sep='\t')
+            if len(elements) != 5:
+                continue
+
+            rel, source, target, js  = elements[1:5]
+
+            lang_source, source_w = source.split('/')[2:4]
+            lang_target, target_w = target.split('/')[2:4]
+
+            source_w = " ".join(source_w.split("_"))
+            target_w = " ".join(target_w.split("_"))
+
+            if rel in ['/r/RelatedTo', '/r/IsA', '/r/PartOf', '/r/HasA', '/r/UsedFor', '/r/CapableOf', '/r/Causes', '/r/HasProperty', '/r/Desires', '/r/Synonym', '/r/Antonym', '/r/Entails', '/r/DistinctFrom', '/r/MannerOf', '/r/HasPrerequisite', '/r/SimilarTo', '/r/InstanceOf']:
+                # for start in ['the ', 'an ', 'a ']:
+                #     if source_w.startswith(start):
+                #         source_w = source_w[len(start):]
+                #     if target_w.startswith(start):
+                #         target_w = target_w[len(start):]
+
+                if lang_source == "en" and lang_target == "en":
+
+                    R[source_w][rel].add(target_w)
+
+                    if rel in symmetric_rels:
+
+                        R[target_w][rel].add(source_w)
+
+        for w in R:
+            R[w] = dict(R[w])
+
+            for r in R[w]:
+                R[w][r] = list(R[w][r])
+
         with open("./relations_dict", 'wb') as f:
             pkl.dump(dict(R), f)
+
+getRelationDict()
