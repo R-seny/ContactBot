6dac76f7ab3233e7d4d12c07b98473f3ff342514
keleas <grishin.na@phystech.edu>
2019-06-26 05:25:18 +0300

    upd code

--- tg_bot.py
+++ tg_bot.py
@@ -1,14 +1,30 @@
 import telebot
 
-bot = telebot.TeleBot("855197924:AAHkIDeoWI_nypSi7SBTjXy8YJMP2OSRv5Y")
+bot = telebot.TeleBot()
 
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
+    print(message.text)
     if message.text == "Hi":
-        bot.send_message(message.from_user.id, "Hello! I am Bot. How can i help you?")
+        bot.send_message(message.chat.id, "Hello! I am Bot. How can i help you?")
     else:
-        bot.send_message(message.from_user.id, "Sorry, i dont understand you.")
+        bot.send_message(message.chat.id, "Sorry, i dont understand you.")
 
 
-bot.polling(none_stop=True, interval=0)
+def listener(messages):
+    """
+    When new messages arrive TeleBot will call this function.
+    """
+    for m in messages:
+        chatid = m.chat.id
+        if m.content_type == 'text':
+            text = m.text
+            bot.send_message(chatid, text)
+            bot.send_message(chatid, 'test')
+
+
+bot.set_update_listener(listener) #register listener
+
+bot.polling(none_stop=True, interval=0, timeout=20)
+
